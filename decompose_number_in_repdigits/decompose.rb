2b70bf6700d31ff54bca40e246bd8122d87f14bb
Cristi Lupascu <cristi.lupascu@gmail.com>
2014-10-29 18:49:13 +0200

    ruby solution for Decomposing numbers into sum of repdigits

---
+++ decompose_number_in_repdigits/decompose.rb
@@ -0,0 +1,33 @@
+require 'rspec/autorun'
+
+class Decomposer
+  def self.closest_repdigit n
+    ((a=n.to_s)[0]*a.size).to_i
+  end
+
+  def self.decompose n
+    terms = []
+    crt_value = n
+
+    while crt_value != 0
+      term = closest_repdigit(crt_value.abs)*(0<=>crt_value)
+      terms << term
+      crt_value += term
+    end
+    terms
+  end
+end
+
+RSpec.describe Decomposer do
+  describe '#closest_repdigit' do
+    it { expect(Decomposer.closest_repdigit(24192)).to eql 22222 }
+    it { expect(Decomposer.closest_repdigit(199)).to eql 111 }
+    it { expect(Decomposer.closest_repdigit(7)).to eql 7 }
+  end
+
+  describe '#decompose' do
+    it { expect(Decomposer.decompose(7)).to eql [-7] }
+    it { expect(Decomposer.decompose(24192)).to eql [-22222, -1111, -888, +22, +7] }
+    it { expect(Decomposer.decompose(113)).to eql [-111, -2] }
+  end
+end
