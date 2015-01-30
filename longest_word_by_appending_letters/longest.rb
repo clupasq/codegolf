abbd5bd2afa6e05bf54561df06cc81549f5d645d
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-01-30 22:50:04 +0200

    fixed longest word by app letters - words can grow in both directions

--- longest_word_by_appending_letters/longest.rb
+++ longest_word_by_appending_letters/longest.rb
@@ -2,6 +2,23 @@
 
 h={}
 open('voc').map{|l|h[l.strip]=1}
-v=->w{w<?a||h[w]&&v[w.chop]}
+v=->w{w<?a||h[w]&&(v[w.chop]||v[w[1..-1]])}
 p h.keys.max_by{|w|w.size*(v[w]?1:0)}
+
+=begin
+  
+Longest 10:
+
+["brahmanists",
+ "franciscans",
+ "carabineros",
+ "brahmanisms",
+ "prestating",
+ "prescripts",
+ "carabinero",
+ "bolivianos",
+ "carabiners",
+ "brahmanism"]
+
+=end
 
