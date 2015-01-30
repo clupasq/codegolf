b8d40dc9545ce738c0f1e7e474c18a2dc879f1ea
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-01-30 10:37:38 +0200

    ruby solution to longest chain word challenge

---
+++ longest_word_by_appending_letters/longest.rb
@@ -0,0 +1,9 @@
+# http://codegolf.stackexchange.com/questions/45168/find-the-longest-word-that-can-be-constructed-by-appending-single-letters-to-exi
+
+h={}
+open('voc').map{|l|h[l.strip]=1}
+v=->w{w<?a||h[w]&&v[w.chop]}
+p h.keys.select{|w|v[w]}.max_by &:size
+
+
+
