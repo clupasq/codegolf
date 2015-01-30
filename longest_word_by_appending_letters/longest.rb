de44a088ebe3f8ff63900372aaecea095c90724b
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-01-30 23:28:34 +0200

    saved some chars by avoiding expensive .reverse

--- longest_word_by_appending_letters/longest.rb
+++ longest_word_by_appending_letters/longest.rb
@@ -2,8 +2,8 @@
 
 h={}
 open('voc').map{|l|h[l.strip]=1}
-v=->w,c=[]{w<?a?c:h[w]&&(v[w.chop,z=c+[w]]||v[w[1..-1],z])}
-puts h.map{|k,_|(v[k]||[]).reverse.join ?\n}.max_by &:size
+v=->w,c=[]{w<?a?c:h[w]&&(v[w.chop,z=[w]+c]||v[w[1..-1],z])}
+puts h.map{|k,_|(v[k]||[]).join ?\n}.max_by &:size
 
 # p h.keys.select{|w|v[w]}.compact.first 10
 
