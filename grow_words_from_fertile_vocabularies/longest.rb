5c620840d56ff7870e41c8765772b07ff58ced74
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-01-31 10:29:08 +0200

    took input from stdin

--- grow_words_from_fertile_vocabularies/longest.rb
+++ grow_words_from_fertile_vocabularies/longest.rb
@@ -1,12 +1,16 @@
 # http://codegolf.stackexchange.com/questions/45168/find-the-longest-word-that-can-be-constructed-by-appending-single-letters-to-exi
 
 h={}
-open('voc').map{|l|h[l.strip]=1}
+$<.map{|l|h[l.strip]=1}
 v=->w,c=[]{w<?a?c:h[w]&&(v[w.chop,z=[w]+c]||v[w[1..-1],z])}
 puts h.map{|k,_|(v[k]||[]).join ?\n}.max_by &:size
 
-# p h.keys.select{|w|v[w]}.compact.first 10
 
+# Commandline:
+# cat voc | ruby longest.rb
+
+
+# p h.keys.select{|w|v[w]}.compact.first 10
 
 =begin
   
