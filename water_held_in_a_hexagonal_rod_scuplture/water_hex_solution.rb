fe5387881e1d6a9623dd0f302c3061f79d1253f3
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-03-02 22:15:26 +0200

    wrote minitest specs

--- water_held_in_a_hexagonal_rod_scuplture/water_hex_solution.rb
+++ water_held_in_a_hexagonal_rod_scuplture/water_hex_solution.rb
@@ -1,6 +1,5 @@
-# coding: utf-8
-
 # http://codegolf.stackexchange.com/questions/47229/water-held-in-a-hexagonal-rod-scuplture
+require 'minitest/autorun'
 require 'pp'
 
 
@@ -14,16 +13,70 @@ Rod = Struct.new :x,:y,:height,:water_height,:neighbors do
   end
 end
 
-input = '  04  04
-04  01  03
-  04  04'
+f=->input{
+  list = []
 
-input = '55  34  45  66
+  l = input.lines
+
+  (0...l.size).map{ |y|
+    x=-1
+    list += l[y].scan(/../).map{|v| Rod.new x=x+1,y,v.to_i, v.to_i<1?0:99}
+  }
+  g=->x,y{list.find{|r|r.x==x&&r.y==y}||Rod.new(0,0,0,0)}
+
+  list.each do |rod|
+    rod.neighbors = [[-1,-1],[1,-1],[-2,0],[2,0],[1,-1],[1,1]].map{|w,z| g[rod.x+w,rod.y+z]}
+  end
+
+  changed=1
+
+  adjust_water=->r{
+    m=r.neighbors.map(&:total_height).min
+    if r.total_height > m && r.water_height > 0
+      r.water_height = [0,m-r.height].max
+      changed=1
+    end
+  }
+
+  # pp g[1, 2].neighbors[1]
+  # pp g[2, 1].neighbors.map(&:to_s)
+
+
+  (changed=!0
+    list.map{|rod| adjust_water[rod]}
+  )while changed
+
+  list.map(&:water_height).reduce :+
+}
+
+
+describe '#f' do
+
+  it 'passes test case #1' do
+    input = <<-EOS
+  04  04
+04  01  03
+  04  04
+EOS
+    
+    assert_equal 2, f[input]
+  end
+
+  it 'passes test case #2' do
+    input = <<-EOS
+55  34  45  66
   33  21  27
 23  12  01  77
-  36  31  74'
+  36  31  74
+EOS
 
-input = '        35  36  77  22                      23  32  54  24
+    assert_equal 35, f[input]
+  end
+
+
+  it 'passes test case #3' do
+    input = <<-EOS
+        35  36  77  22                      23  32  54  24
       33  07  02  04  21                  54  07  07  07  76
     20  04  07  07  01  20              54  11  81  81  07  76
   20  67  67  22  07  01  78          54  07  81  07  81  09  76
@@ -40,39 +93,8 @@ input = '        35  36  77  22                      23  32  54  24
     50                      20  02  48
   70                          56  20
                                 90
-'
+EOS
 
-list = []
-
-l = input.lines
-
-(0...l.size).map{ |y|
-  x=-1
-  list += l[y].scan(/../).map{|v| Rod.new x=x+1,y,v.to_i, v.to_i<1?0:99}
-}
-g=->x,y{list.find{|r|r.x==x&&r.y==y}||Rod.new(0,0,0,0)}
-
-list.each do |rod|
-  rod.neighbors = [[-1,-1],[1,-1],[-2,0],[2,0],[1,-1],[1,1]].map{|w,z| g[rod.x+w,rod.y+z]}
-end
-
-changed=1
-
-adjust_water=->r{
-  m=r.neighbors.map(&:total_height).min
-  if r.total_height > m && r.water_height > 0
-    r.water_height = [0,m-r.height].max
-    changed=1
+    assert_equal 1432, f[input]
   end
-}
-
-# pp g[1, 2].neighbors[1]
-# pp g[2, 1].neighbors.map(&:to_s)
-
-
-(changed=!0
-  list.map{|rod| adjust_water[rod]}
-)while changed
-
-p list.map(&:water_height).reduce :+
-
+end
