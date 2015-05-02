2857b91fcf83c277dafba781e1bb9381a1fc8b50
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-05-02 12:20:08 +0300

    finding-pairs-for-letters - golfed

--- finding-pairs-for-letters/finding-pairs-for-letters-golfed.rb
+++ finding-pairs-for-letters/finding-pairs-for-letters-golfed.rb
@@ -1,44 +1,22 @@
 # coding: utf-8
-# http://codegolf.stackexchange.com/questions/49517/finding-pairs-for-letters
 require 'minitest/autorun'
 
-F=->s{
-  l=s.split ?\n
-  m=l.size
-  n=l[0].size
-
-  # Make an array with lowercase letters and their coords (`b`)
-  # Make a hash that for each target (uppercase letter) holds
-  # its coordinates
-  b=[]
-  t={}
-  [*0...m].product([*0...n]).map{|y,x| 
-    c=l[y][x]
-    c!=' '&&(c>?Z?b<<[c,y,x]:t[c.downcase]=[y,x])
-  }
-  
-  # `l` is a function that, given an array of the form
-  # [lowercase_letter, y-coord, x-coord] will append to that array
-  # the number of steps required for the letter to reach its
-  # uppercase correspondent
-  l=->q{
-    c,y,x=q
-    u,i=t[c]
-    d=c.ord-97
-    (m*n).times{|w|
-      e=([u-y,i-x].map &:abs).max
-      e<=d&&q<<w+e
-      y=(y+1)%m
-      x=(x+1)%n
-    }
-  }
-
-  # apply function `l` to each of the lowercase letter...
-  b.map &l
-
-  # ...then sort accordingly and return the array
-  b.map{|x|[x[3],x[0]]}.select{|a,b|a}.sort.map{|a,b|b}
-}
+F=->s{l=s.split ?\n
+m=l.size
+n=l[0].size
+b=[]
+t={}
+[*0...m].product([*0...n]).map{|y,x|c=l[y][x]
+c!=' '&&(c>?Z?b<<[c,y,x]:t[c.downcase]=[y,x])}
+l=->q{c,y,x=q
+u,i=t[c]
+d=c.ord-97
+(m*n).times{|w|e=([u-y,i-x].map &:abs).max
+e<=d&&q<<w+e
+y=(y+1)%m
+x=(x+1)%n}}
+b.map &l
+b.map{|x|[x[3],x[0]]}.select{|a,b|a}.sort.map{|a,b|b}}
 
 
 describe '#F' do
