6b87d42bea2448b6b64afbce87334c3daab10923
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-07-24 17:35:07 +0300

    avoided comparison with -1 to save 1 char

--- 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
+++ 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
@@ -8,7 +8,7 @@ c=->s{(0...2**s.scan(y=/\|+/).size).map{|i|x=-1;s.gsub(y){|r|i[x+=1]<1?r:'\/'*r.
 s=r=t.size
 i=m=t[/ +/].size
 c[t].map{|x|d=i+s-x.size
-d>-1&&d<m&&(r=x.gsub(/ +/,' '*m=d))}
+d<0||d<m&&(r=x.gsub(/ +/,' '*m=d))}
 r}
 
 describe K do 
