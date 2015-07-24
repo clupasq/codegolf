2962103a14857ce0a8ed806811f2e9b625e206a4
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-07-24 17:47:16 +0300

    merged two `.map`s that were next to each other

--- 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
+++ 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
@@ -5,7 +5,8 @@ require 'minitest/autorun'
 K=
 ->t{s=r=t.size
 i=m=t[o=/ +/].size
-(0...2**t.scan(y=/\|+/).size).map{|i|x=-1;t.gsub(y){|r|i[x+=1]<1?r:'\/'*r.size}}.map{|x|d=i+s-x.size
+(0...2**t.scan(y=/\|+/).size).map{|n|q=-1;x=t.gsub(y){|r|n[q+=1]<1?r:'\/'*r.size}
+d=i+s-x.size
 d<0||d<m&&r=x.gsub(o,' '*m=d)}
 r}
 
