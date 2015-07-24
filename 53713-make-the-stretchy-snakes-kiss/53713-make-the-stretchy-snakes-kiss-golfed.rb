36bab53f3f78391acd6800d71747050375e6da3d
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-07-24 17:44:23 +0300

    small golf improvements: assigned variable for reused regex; removed useless parens

--- 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
+++ 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
@@ -3,11 +3,10 @@ require 'pp'
 require 'minitest/autorun'
 
 K=
-->t{
-s=r=t.size
-i=m=t[/ +/].size
+->t{s=r=t.size
+i=m=t[o=/ +/].size
 (0...2**t.scan(y=/\|+/).size).map{|i|x=-1;t.gsub(y){|r|i[x+=1]<1?r:'\/'*r.size}}.map{|x|d=i+s-x.size
-d<0||d<m&&(r=x.gsub(/ +/,' '*m=d))}
+d<0||d<m&&r=x.gsub(o,' '*m=d)}
 r}
 
 describe K do 
