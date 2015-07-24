60c49d50bc44bc7d4173f53b165c3d3dad9c883e
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-07-24 17:36:04 +0300

    inlined stabby lambda that was used only once

--- 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
+++ 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
@@ -4,10 +4,9 @@ require 'minitest/autorun'
 
 K=
 ->t{
-c=->s{(0...2**s.scan(y=/\|+/).size).map{|i|x=-1;s.gsub(y){|r|i[x+=1]<1?r:'\/'*r.size}}}
 s=r=t.size
 i=m=t[/ +/].size
-c[t].map{|x|d=i+s-x.size
+(0...2**t.scan(y=/\|+/).size).map{|i|x=-1;t.gsub(y){|r|i[x+=1]<1?r:'\/'*r.size}}.map{|x|d=i+s-x.size
 d<0||d<m&&(r=x.gsub(/ +/,' '*m=d))}
 r}
 
