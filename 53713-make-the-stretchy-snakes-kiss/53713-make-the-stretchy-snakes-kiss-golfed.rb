8d3d60eda9529419559ff67729f464a6ee0bcd19
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-07-24 17:52:25 +0300

    split two lines of golfed code & erased `;`

--- 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
+++ 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
@@ -5,7 +5,8 @@ require 'minitest/autorun'
 K=
 ->t{s=r=t.size
 i=m=t[o=/ +/].size
-(0...2**t.scan(y=/\|+/).size).map{|n|q=-1;x=t.gsub(y){|r|n[q+=1]<1?r:'\/'*r.size}
+(0...2**t.scan(y=/\|+/).size).map{|n|q=-1
+x=t.gsub(y){|r|n[q+=1]<1?r:'\/'*r.size}
 d=i+s-x.size
 d<0||d<m&&r=x.gsub(o,' '*m=d)}
 r}
