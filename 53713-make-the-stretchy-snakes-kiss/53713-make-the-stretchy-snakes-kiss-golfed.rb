f818356cd5d937b5802ca41d553123889c3dea6a
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-07-24 16:36:20 +0300

    fixed minor golfing mistakes

--- 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
+++ 53713-make-the-stretchy-snakes-kiss/53713-make-the-stretchy-snakes-kiss-golfed.rb
@@ -3,8 +3,7 @@ require 'pp'
 require 'minitest/autorun'
 
 K=
-->t{
-c=->s{(0...2**s.scan(/(\|+)/).size).map{|i|x=-1;s.gsub(/\|+/){|r|i[x+=1]<1?r:'\/'*r.size}}}
+->t{c=->s{(0...2**s.scan(y=/\|+/).size).map{|i|x=-1;s.gsub(y){|r|i[x+=1]<1?r:'\/'*r.size}}}
 a,b=r=t.split
 s=m=t.size
 c[a].product(c[b]){|a,b|d=(a+b).size-s
