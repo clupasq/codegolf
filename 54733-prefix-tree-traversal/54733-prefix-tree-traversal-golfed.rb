f80a9e58442f3cdd1c68f3d9d764bc53e38fa8af
Cristian Lupascu <cristi.lupascu@gmail.com>
2015-08-15 23:29:50 +0300

    golfed 54733-prefix-tree-traversal

---
+++ 54733-prefix-tree-traversal/54733-prefix-tree-traversal-golfed.rb
@@ -0,0 +1,9 @@
+gets = "cat[s[up[][]][]ch[e[r[]s[]]]a[maran[]comb[]pult[[]ing[]]]]\n"
+# gets = "[donut[][]cruller[]]\n"
+# gets = "[]\n"
+# gets = "[[[[[]]]]]\n"
+
+t=['']
+l=[0]
+gets.chars.map{|c|c<?]?t<<''&&(l<<0)[-2]+=1:c<?^?(x=l.pop;t.pop==''&&(puts t*''if x<1;t[-1]='')):t[-1]<<c}
+
