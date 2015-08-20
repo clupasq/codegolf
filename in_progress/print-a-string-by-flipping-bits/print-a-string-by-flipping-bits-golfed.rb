c31cb4103d5adacf6da52a25a99b295d2453169c
Cristian Lupascu <cristi.lupascu@gmail.com>
2015-08-21 00:14:42 +0300

    removed whitespace; extracted `z` range definition to avoid extra parens

--- in_progress/print-a-string-by-flipping-bits/print-a-string-by-flipping-bits-golfed.rb
+++ in_progress/print-a-string-by-flipping-bits/print-a-string-by-flipping-bits-golfed.rb
@@ -1,13 +1,13 @@
 # pending solution for http://meta.codegolf.stackexchange.com/a/5732/3527
 
 F=
-#205
+#201
 ->w{s=[0]*8
 q=[]
-w.chars{|c|
-*m,i=(z=(0..7)).map{|j|z.select{|k|s[k]!=z.map{|i|c.ord[i]}.rotate(j)[k]}+[j]}.min_by &:size
-q+=m.map{|m|s[m]=1-s[m];"t #{7-m}"}+["p #{i}"]
-}
+w.chars{|c|z=0..7
+*m,i=z.map{|j|z.select{|k|s[k]!=z.map{|i|c.ord[i]}.rotate(j)[k]}+[j]}.min_by &:size
+q+=m.map{|m|s[m]=1-s[m]
+"t #{7-m}"}+["p #{i}"]}
 puts ["#{q.size} instructions",q]}
 
 
