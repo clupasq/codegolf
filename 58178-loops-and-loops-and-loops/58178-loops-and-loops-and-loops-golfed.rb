ba7617f552762523e1e1921c6df95a5b600d7824
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-09-18 11:15:51 +0300

    removed whitespace

--- 58178-loops-and-loops-and-loops/58178-loops-and-loops-and-loops-golfed.rb
+++ 58178-loops-and-loops-and-loops/58178-loops-and-loops-and-loops-golfed.rb
@@ -3,26 +3,14 @@ F=
 ->i{n=Hash.new{|h,k|h[k]=[0,p]}
 t=y=0
 i.lines{|l|x=0
-  l.chars{|c|x+=1
-
-    i='><v^'.index c
-
-    if i
-      s,d=[[[y,x-4],[y,x+2]],[[y,x+4],[y,x-2]],[[y-3,x],[y+1,x]],[[y+3,x],[y-1,x]]][i]
-      (n[s][1]=n[d])[0]+=1
-      # n[d]
-    end
-  }
-  y+=1
-}
-
+l.chars{|c|x+=1
+q='><v^'.index c
+q&&(s,d=[[[y,x-4],[y,x+2]],[[y,x+4],[y,x-2]],[[y-3,x],[y+1,x]],[[y+3,x],[y-1,x]]][q];(n[s][1]=n[d])[0]+=1)}
+y+=1}
 c,*_,s=n.values.sort_by{|v|v[0]}
-
 l=n.size
 s[0]>1?((t+=1;c=c[1])while c!=s):t=l-=1
-[t,l-t]
-}
-
+[t,l-t]}
 
 
 require 'minitest/autorun'
