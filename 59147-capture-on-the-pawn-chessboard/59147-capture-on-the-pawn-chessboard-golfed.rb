28981ca172e3f2a28ba721bb96fce4338fa42b59
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-09-30 16:52:23 +0300

    golfed #parse_line

---
+++ 59147-capture-on-the-pawn-chessboard/59147-capture-on-the-pawn-chessboard-golfed.rb
@@ -0,0 +1,55 @@
+
+def parse_line(s)
+  l = {}
+  i = 0
+  s.chars.map{|c|
+    case c 
+    when '1'..'8'
+      i += c.to_i
+    else
+      l[i] = c
+      i += 1
+    end
+  }
+
+  l
+end
+
+IS_CAPTURE_POSSIBLE=
+->board{
+  last_line = {}
+
+  board.split('/').each do |line_str|
+    line = parse_line line_str
+
+    return true if (0..8).any?{ |i| last_line[i] == 'p' && (line[i-1] == 'P' || line[i+1] == 'P')  }
+
+    last_line = line
+  end
+
+  false
+}
+
+
+
+require 'minitest/autorun'
+
+describe :IS_CAPTURE_POSSIBLE do
+  it('recognizes when capture is possible') do 
+    assert IS_CAPTURE_POSSIBLE['8/7p/6P1/8/8/8/8/8']
+    assert IS_CAPTURE_POSSIBLE['8/8/p7/1P6/3P3p/8/8/8']
+    assert IS_CAPTURE_POSSIBLE['8/2P5/8/4P1p1/2p2P2/3p4/3p1P2/8']
+    assert IS_CAPTURE_POSSIBLE['8/P7/8/5P2/2pp4/3P2p1/3pP3/8']
+    assert IS_CAPTURE_POSSIBLE['8/P7/p7/p1P1P3/1P3p2/8/1p6/8']
+    assert IS_CAPTURE_POSSIBLE['8/4p1P1/2P2P1P/2p1pPpp/8/6P1/pP1p4/8']
+  end
+
+  it('recognizes when capture is impossible') do 
+    refute IS_CAPTURE_POSSIBLE['8/8/8/8/8/8/8/8']
+    refute IS_CAPTURE_POSSIBLE['8/7P/6p1/8/8/8/8/8']
+    refute IS_CAPTURE_POSSIBLE['8/7p/7P/8/8/8/8/8']
+    refute IS_CAPTURE_POSSIBLE['8/pppppppp/8/8/8/8/PPPPPPPP/8']
+    refute IS_CAPTURE_POSSIBLE['8/p7/8/1p6/5P2/8/8/8']
+    refute IS_CAPTURE_POSSIBLE['8/p7/P7/2P1p1p1/2p5/8/PP6/8']
+  end
+end
