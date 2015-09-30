ed1ad691b5f18eb93fa5e085d0f6b0e790d1a593
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-09-30 17:03:48 +0300

    inlined #parse_line

--- 59147-capture-on-the-pawn-chessboard/59147-capture-on-the-pawn-chessboard-golfed.rb
+++ 59147-capture-on-the-pawn-chessboard/59147-capture-on-the-pawn-chessboard-golfed.rb
@@ -1,29 +1,11 @@
-
-def parse_line(s)
-  r = {}
-  i = 0
-  s.chars.map{|c|
-    case c 
-    when '1'..'8'
-      i += c.to_i
-    else
-      r[i] = c
-      i += 1
-    end
-  }
-  r
-end
-
 IS_CAPTURE_POSSIBLE=
-->board{
-  l={}
-  board.split('/').map{|s|
-    c=parse_line s
-    return true if (0..8).any?{ |i| l[i] == 'p' && (c[i-1] == 'P' || c[i+1] == 'P')}
-    l=c
-  }
-  p
-}
+->board{l={}
+board.split('/').map{|s|c={}
+i=0
+s.chars.map{|x|n=x.to_i;c[i]=x;i+=n<1?1:n}
+return true if (0..8).any?{ |i| l[i] == 'p' && (c[i-1] == 'P' || c[i+1] == 'P')}
+l=c}
+p}
 
 
 
