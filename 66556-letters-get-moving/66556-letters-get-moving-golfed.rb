7799dba20ed9b4d1bfccca71a2ed3282e6ded161
Cristi Lupascu <cristi.lupascu@gmail.com>
2015-12-14 12:15:56 +0200

    removed `MovableChar`; one letter vars

---
+++ 66556-letters-get-moving/66556-letters-get-moving-golfed.rb
@@ -0,0 +1,33 @@
+# rubocop: disable all
+require 'minitest/autorun'
+
+def move(s)
+a=s.chars.map{|c|[c,c=~/[a-z]/i&&c.upcase.ord-64]}
+
+while i=a.find_index{|c|c[1]}
+c=a.delete_at i
+n=(i+c[1])%(a.size+1)
+c[1]=p
+a.insert(n,c)
+end
+
+a.join
+end
+
+describe :MovingLetters do
+  def test_case_1
+    assert_equal 'tca', move('cat')
+  end
+
+  def test_case_2
+    assert_equal '.F.NU', move('F.U.N')
+  end
+
+  def test_case_3
+    assert_equal 'goD', move('Dog')
+  end
+
+  def test_case_4
+    assert_equal '*hibey', move('hi*bye')
+  end
+end
