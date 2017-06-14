af04a48ad1588bea1686978e5ec2f918966b4f0e
Cristian Lupascu <cristi.lupascu@gmail.com>
2017-06-14 15:55:34 +0300

    Golfed ASCII boxes

--- 126305-draw-ascii-boxes-in-boxes/ascii_boxes.rb
+++ 126305-draw-ascii-boxes-in-boxes/ascii_boxes.rb
@@ -1,26 +1,28 @@
 require 'minitest/autorun'
 require_relative '../test_utils'
 
-# --- {START COUNTING}
-f = ->(a,b,c){
-  g = a.times.map{?#*a}
 
-  x=(a-b)/2
-  r=[*x...a-x]
-  r.product (r) do |i|
-    m,n=i
-    g[m][n]=?*
-  end
-
-  x=(a-c)/2
-  r=[*x...a-x]
-  r.product (r) do |i|
-    m,n=i
-    g[m][n]=?@
-  end
-
-  g
+f=->(a,b,c){
+  q=(a-b)/2
+  q=(q...a-q)
+  v=(a-c)/2
+  v=(v...a-v)
+  (0...a*a).map{|i|
+    y=i/a
+    x=i%a
+    v===y && v===x ? ?@  : (  q===y && q===x  ? ?* : ?# ) + (x==a-1??\n:'')
+  }.join
 }
+
+# --- {START COUNTING}
+
+f=->(a,b,c){o=a.times.map{?#*a}
+g=->(y,c){x=(a-y)/2
+r=[*x...a-x];r.product(r){|i|m,n=i
+o[m][n]=c}}
+g[b,?*]
+g[c,?@]
+o}
 
 # --- {END COUNTING}
 
@@ -56,6 +58,4 @@ EOS
 end
 
 print_size_stats
-
-
 
