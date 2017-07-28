55e2f49dde1e4c95439050de682f452c1170a3f4
Cristian Lupascu <cristi.lupascu@gmail.com>
2017-07-28 11:31:06 +0300

    Golfed 136318-generating-fermat-primes

--- 136318-generating-fermat-primes/136318-generating-fermat-primes.hs
+++ 136318-generating-fermat-primes/136318-generating-fermat-primes.hs
@@ -6,6 +6,13 @@ isPrime n = and[mod n i>0|i<-[2..n-1]]
 fermat :: Int -> Int
 fermat=(filter isPrime [2^(2^i)+1|i<-[0..]]!!)
 
+-- Not posted because xnor has a better approach
+-- using Pepin's test: https://codegolf.stackexchange.com/a/136322/3527
+
+-- START COUNTING
+f=([x|x<-[2^(2^i)+1|i<-[0..]],and[mod x j>0|j<-[2..x-1]]]!!)
+-- END COUNTING
+
 main = hspec $ do
   describe "Fermat" $ do
     it "detects primes" $ do
@@ -26,6 +33,13 @@ main = hspec $ do
       fermat 2 `shouldBe` 17
       fermat 3 `shouldBe` 257
       fermat 4 `shouldBe` 65537
+
+    it "finds Fermat numbers - golfed" $ do
+      f 0 `shouldBe` 3
+      f 1 `shouldBe` 5
+      f 2 `shouldBe` 17
+      f 3 `shouldBe` 257
+      f 4 `shouldBe` 65537
 
 
 
